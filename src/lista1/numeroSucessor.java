dd8f4fcd7903fca93bd81ef52a1abc8c0e329c4b
will-castilho <willian.cp@edu.udesc.br>
2024-05-20 20:32:25 -0300

    Commit inicial

---
+++ src/lista1/numeroSucessor.java
@@ -0,0 +1,19 @@
+package lista1;
+
+import java.util.Scanner;
+
+public class numeroSucessor {
+    int num;
+    int suc;
+    int ant;
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(system.in);
+
+        System.out.println("Escreva um numero: ");
+        num = ler.nextInt();
+
+        System.out.println("Numero digitado: %d\n ",num);
+
+    }
+}
